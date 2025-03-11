f3ec858f3e3546ba769211dde3734aff5424df85
fffilps <jmariwyatt@gmail.com>
2025-03-11 04:12:39 -0700

    added cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+# Init App
+app = FastAPI(title="Real-Time Task List API")
+
+# CORS middleware to control access
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api/hello")
 def hello():
